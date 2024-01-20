f8ed89425074646ce9bae9a12daa6d49b0cab903
samwhelp <samwhelp@users.noreply.github.com>
2024-01-20 08:41:04 +0800

    fix

---
+++ prototype/V6/kde-config/locale/en_us/Breeze-Dark/config-install.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+################################################################################
+### Head: kde
+##
+
+kde_config_install () {
+
+	echo
+	echo "##"
+	echo "## Config: kde"
+	echo "##"
+	echo
+
+
+	kde_config_install_by_dir
+
+
+	echo
+
+}
+
+kde_config_install_by_dir () {
+
+
+	echo
+	echo "mkdir -p ${HOME}"
+	mkdir -p "${HOME}"
+
+
+	echo
+	echo "cp -rf ./asset/overlay/etc/skel/. ${HOME}"
+	cp -rf "./asset/overlay/etc/skel/." "${HOME}"
+
+
+}
+
+##
+### Tail: kde
+################################################################################
+
+
+################################################################################
+### Head: config_install
+##
+
+main_config_install () {
+
+	kde_config_install
+
+}
+
+##
+### Tail: config_install
+################################################################################
+
+
+################################################################################
+### Head: Main
+##
+
+__main__ () {
+
+	main_config_install
+
+}
+
+##
+## Start
+##
+__main__
+
+##
+### Tail: Main
+################################################################################
