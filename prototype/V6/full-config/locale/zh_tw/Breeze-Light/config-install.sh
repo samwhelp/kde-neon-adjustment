713766448c62a689aa138e1a54d9f697f98d96b0
samwhelp <samwhelp@users.noreply.github.com>
2024-01-21 09:39:40 +0800

    fix

--- prototype/V6/full-config/locale/zh_tw/Breeze-Light/config-install.sh
+++ prototype/V6/full-config/locale/zh_tw/Breeze-Light/config-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#set -e
+set -e
 
 
 ################################################################################
@@ -9,7 +9,7 @@
 
 kde_config_run_pre () {
 
-	kde_service_stop
+	#kde_service_stop
 
 
 	return 0
@@ -18,7 +18,7 @@ kde_config_run_pre () {
 
 kde_config_run_post () {
 
-	kde_service_start
+	#kde_service_start
 
 
 	return 0
